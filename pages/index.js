157b567559dd3da5de217eccd254d957ba71e9a6
Freddy Jimenez <freddy.jimenez@typeform.com>
2021-04-10 18:37:08 +0200

    first commit

--- pages/index.js
+++ pages/index.js
@@ -1,65 +1,95 @@
-import Head from 'next/head'
-import styles from '../styles/Home.module.css'
+import Head from "next/head";
+import {
+  GithubIcon,
+  InstagramIcon,
+  LinkedinIcon,
+  TwitterIcon,
+} from "../components/Icons";
+import styles from "../styles/Home.module.css";
 
 export default function Home() {
   return (
-    <div className={styles.container}>
-      <Head>
-        <title>Create Next App</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-
-      <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
-
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
-        </p>
-
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
+    <section className={styles.site}>
+      <section className={styles.container}>
+        <Head>
+          <title>Freddy Jimenez</title>
+          <link rel="icon" href="/favicon.ico" />
+        </Head>
+        <main className={styles.main}>
+          <h1 className={styles.name}>Hi! my name is Freddy</h1>
+          <p>
+            I'm a{" "}
+            <strong className={`${styles.highlight} ${styles.job}`}>
+              Software Engineer{" "}
+            </strong>
+            passionate about frontend technologies.
+          </p>
+          <p>
+            Currently helping to connect people through more meaningful
+            conversations at
+            <a
+              className={`${styles.highlight} ${styles.work}`}
+              target="_blank"
+              href="https://www.typeform.com/"
+            >
+              Typeform
+            </a>
+          </p>
+          <article className={styles.hobbies}>
+            <p>I like </p>
+            <ul className={styles.dynamicText}>
+              <li className={styles.travel}>
+                <span>travel</span>
+              </li>
+              <li className={styles.running}>
+                <span>running</span>
+              </li>
+              <li className={styles.create}>
+                <span>create cool stuff</span>
+              </li>
+              <li className={styles.drums}>
+                <span>play the drums</span>
+              </li>
+              <li className={styles.beach}>
+                <span>the beach</span>
+              </li>
+              <li className={styles.videogames}>
+                <span>videogames</span>
+              </li>
+            </ul>
+          </article>
+        </main>
+        <footer className={styles.footer}>
           <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className={styles.card}
+            className={styles.social}
+            target="_blank"
+            href="https://github.com/alfrejivi"
           >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
+            <GithubIcon />
           </a>
-
           <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
+            className={styles.social}
+            target="_blank"
+            href="https://twitter.com/alfrejivi"
           >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
+            <TwitterIcon />
           </a>
-        </div>
-      </main>
-
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
-        </a>
-      </footer>
-    </div>
-  )
+          <a
+            className={styles.social}
+            target="_blank"
+            href="https://www.linkedin.com/in/alfrejivi/"
+          >
+            <LinkedinIcon />
+          </a>
+          <a
+            className={styles.social}
+            target="_blank"
+            href="https://www.instagram.com/alfrejivi/"
+          >
+            <InstagramIcon />
+          </a>
+        </footer>
+      </section>
+    </section>
+  );
 }
